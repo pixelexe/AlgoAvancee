7c4c4ebcbe51380f3e9bdde2c842bc094b31d408
pixelexe <rerezheng@gmail.com>
2024-01-25 10:43:21 +0100

    SonarLint pas content

--- src/test/java/fr/pantheonsorbonne/cri/MyArrayListTest.java
+++ src/test/java/fr/pantheonsorbonne/cri/MyArrayListTest.java
@@ -3,25 +3,21 @@ package fr.pantheonsorbonne.cri;
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
-import java.util.Arrays;
-
 import org.junit.jupiter.api.Test;
 
 /**
  * Unit test for simple App.
  */
-public class MyArrayListTest
+class MyArrayListTest
 {
     /**
      * Rigorous Test :-)
      */
     @Test
-    public void shouldAnswerWithTrue() throws IndexOutOfBoundsException
+    void shouldAnswerWithTrue() throws IndexOutOfBoundsException
     {
         MyArrayList liste = new MyArrayList();
-        assertEquals(liste.toString(), "[]");
+        assertEquals("[]", liste.toString());
         liste.add("a");
         String data1[] = {"a",};
         assertEquals(liste.data.length, data1.length);
@@ -41,15 +37,15 @@ public class MyArrayListTest
         liste.add("f");
         String data6[] = {"a","b","c","d","e","f",null,null};
         assertArrayEquals(liste.data, data6);
-        assertEquals(liste.get(0),"a");
-        assertEquals(liste.get(1),"b");
-        assertEquals(liste.get(2),"c");
-        assertEquals(liste.get(3),"d");
-        assertEquals(liste.get(4),"e");
-        assertEquals(liste.get(5),"f");
+        assertEquals("a",liste.get(0));
+        assertEquals("b",liste.get(1));
+        assertEquals("c",liste.get(2));
+        assertEquals("d",liste.get(3));
+        assertEquals("e",liste.get(4));
+        assertEquals("f",liste.get(5));
         assertThrows(IndexOutOfBoundsException.class,()->liste.get(6));
         assertThrows(IndexOutOfBoundsException.class,()->liste.get(7));
-        assertEquals(liste.toString(), "[a, b, c, d, e, f]");;
+        assertEquals("[a, b, c, d, e, f]", liste.toString());
     }
 
     
