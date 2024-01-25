2589d62df2a529de9ed7a40e0e35fe39da726d9b
Dan Kleczewski <Dan.Kleczewski1@etu.univ-paris1.fr>
2024-01-25 10:08:29 +0100

    Ajout méthodes

--- src/main/java/fr/pantheonsorbonne/cri/MyArrayList.java
+++ src/main/java/fr/pantheonsorbonne/cri/MyArrayList.java
@@ -1,23 +1,25 @@
 package fr.pantheonsorbonne.cri;
 
+import java.util.Arrays;
+
 public class MyArrayList{
     int flag;
     String[] data;
 
-    public MyArrayList(String[] data, int flag) {
-        this.data = data;
-        this.flag = flag;
+    public MyArrayList() {
+        this.data = new String[1];
+        this.flag = 0;
     }
 
     private void ensureCapacity(int n) {
         if (n < data.length) {
             return;
         } else {//
-            String[] tab2 = new String[data.length * 2];// on crée un nouveau tableau de taille double
-            for (int i = 0; i < data.length; i++) {// on copie les éléments du tableau dans le nouveau tableau
-                tab2[i] = data[i];// on copie les éléments du tableau dans le nouveau tableau
+            String[] tab2 = new String[data.length * 2];
+            for (int i = 0; i < data.length; i++) {
+                tab2[i] = data[i];
             }
-            data = tab2;// on remplace le tableau par le nouveau tableau
+            data = tab2;
         }
     }
 
@@ -39,8 +41,34 @@ public class MyArrayList{
         return sb.toString();
     }
 
+    public boolean contains(String value){
+        for (int i = 0; i<this.data.length ; i++){
+            if (this.data[i].equals(value)){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public int indexOf(String value){
+        for (int i = 0; i<this.data.length ; i++){
+            if (this.data[i].equals(value)){
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public boolean isEmpty(){
+        return (Arrays.equals(this.data, new String[1]));
+    }
+
+    public String remove(int index){
+        return "";
+    }
+
     public static void main(String[] args) {
-        MyArrayList liste = new MyArrayList(new String[1], 0);
+        MyArrayList liste = new MyArrayList();
         System.out.println(liste.toString().equals("[]"));
         liste.add("a");
         liste.add("b");
