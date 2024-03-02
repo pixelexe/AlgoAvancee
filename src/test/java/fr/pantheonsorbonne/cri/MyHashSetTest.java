4c0344c99508e1aea8044669ae9d134497c516ab
Dan Kleczewski <dankleczew@gmail.com>
2024-03-02 13:52:17 +0100

    Réparé le HashSet ajouté le test (un peu à l'arrache) et temps d'exec pour HashSet

--- src/test/java/fr/pantheonsorbonne/cri/MyHashSetTest.java
+++ src/test/java/fr/pantheonsorbonne/cri/MyHashSetTest.java
@@ -2,6 +2,7 @@ package fr.pantheonsorbonne.cri;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
@@ -15,9 +16,16 @@ public class MyHashSetTest {
         
         a.add("S");
         assertEquals("S".hashCode()%10, 3);
-        assertEquals("\n\n\nS     |\n\n\n\n\n\n\n", a.toString());
-        //assertFalse(a.add("S"));
-        assertEquals("\n\n\nS     |\n\n\n\n\n\n\n", a.toString());
+        assertEquals("&&&S|&&&&&&&", a.toString());
+        assertFalse(a.add("S"));
+        assertTrue(a.add("A"));
+        assertEquals("&&&S|&&A|&&&&&", a.toString());
+        assertTrue(a.add("B"));
+        assertEquals("&&&S|&&A|&B|&&&&", a.toString());
+        for (int i = 97 ; i < 117; i++){
+            a.add(Character.toString(i));
+        }
+        assertEquals("&&&S|&&&&&&&&&&&&&&a|&b|&c|&d|&e|&f|&g|&h|&A|i|&B|j|&k|&l|&m|&n|&o|&p|&q|&r|&s|&t|&&&&", a.toString());
+        assertTrue(a.contains("s"));
     }
-
 }
